f1cc38515fa86f8198361048c15f9116dca6c3d4
sepertigasurga <sepertigasurga@gmail.com>
2020-11-23 14:39:01 +0700

    Initial commit

---
+++ app/src/main/java/com/adesurahman/mysqlcrud/TampilPegawai.java
@@ -0,0 +1,20 @@
+package com.adesurahman.mysqlcrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class TampilPegawai extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampil_pegawai);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
